6a054b627c1ccd6e47ad634ceaa0fbf4b12137f1
fabianoobispo <fbc623@gmail.com>
2021-01-11 10:06:58 -0300

    alterado banco para mysql local

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,19 @@
 module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'api',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
+/*
+banco em postgres
+module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
@@ -9,8 +24,7 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
   },
-};
-
+}; */
 // estou usuando este banco em docker com o coando
 // docker run --name gerenciadoreventos -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
 // para criar migrations
