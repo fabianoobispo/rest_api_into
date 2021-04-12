a17865f4b9aa9aaab48898bea64b98cc206d83a6
fabianoobispo <fbc623@gmail.com>
2021-04-12 13:58:19 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('Umbler listening on port %s', port);
+  console.log('Servidor rodando em %s', port);
 });
